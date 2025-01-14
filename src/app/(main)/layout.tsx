04374258fabe5135cc670b6dd606aa58539d6caa
Anton Vo <antonlevo0903@gmail.com>
2025-01-14 02:12:34 +0100

    chore: max-w

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <Menu />
-      <main className="p-4">
+      <main className="p-4 max-w-7xl mx-auto">
         <PageTitle />
         {children}
       </main>
