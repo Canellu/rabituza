2ae2c0d88c3b5ec6032fcfc35bce06ecddd6f360
Anton Vo <antonlevo0903@gmail.com>
2025-01-21 23:38:58 +0100

    add exercise button

--- src/app/components/Tracker.tsx
+++ src/app/components/Tracker.tsx
@@ -1,5 +1,37 @@
+"use client";
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Plus } from "lucide-react";
+
 const Tracker = () => {
-  return <div>Tracker</div>;
+  return (
+    <div className="h-full">
+      <Drawer>
+        <DrawerTrigger className="fixed bottom-28 right-8 px-4 p-2 rounded-full bg-primary flex gap-1 items-center justify-center">
+          <Plus className="size-4 text-stone-800" />
+          Add exercise
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>Pick exercises to add</DrawerTitle>
+            <DrawerDescription>Exercise 1</DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <DrawerClose>Add</DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </div>
+  );
 };
 
 export default Tracker;
