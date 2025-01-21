c1abb0b33cd74154e31e252f5828b378cc11ee2e
Anton Vo <antonlevo0903@gmail.com>
2025-01-21 22:00:17 +0100

    rm: vibration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,9 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
 import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { ArrowRight } from "lucide-react";
-import useVibrate from "./hooks/useVibrate";
 
 const Login = () => {
-  const vibrationPattern = [300, 50, 300];
-
-  const vibrate = useVibrate(vibrationPattern);
-
-  const handleVibrate = () => {
-    alert("Bzzz Bzzzz!");
-    vibrate();
-  };
-
   return (
     <div className="flex items-center justify-center h-screen flex-col gap-8">
-      <Button onClick={handleVibrate}>Vibrate</Button>
       <LoginLink
         postLoginRedirectURL="/home"
         className="bg-primary rounded-full px-6 py-3 font-medium flex gap-3"
