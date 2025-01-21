9bc855129f0a1e9ed22301529dc1d13fbea1e380
Anton Vo <antonlevo0903@gmail.com>
2025-01-21 22:51:07 +0100

    fade in

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -4,6 +4,7 @@ import {
   LogoutLink,
   useKindeBrowserClient,
 } from "@kinde-oss/kinde-auth-nextjs";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import RefreshButton from "./RefreshButton";
 import Spinner from "./Spinner";
@@ -13,28 +14,36 @@ const Profile = () => {
 
   return (
     <div className="flex items-center justify-evenly flex-col h-full">
-      <div className="flex items-center flex-col gap-4 ">
-        {!user?.picture ? (
+      <div className="flex items-center flex-col gap-4">
+        {!user ? (
           <Spinner
-            size="size-8"
-            className="size-24 place-items-center place-content-center"
+            size="size-10"
+            className="size-[168px] place-content-center place-items-center"
           />
         ) : (
-          <Image
-            src={user.picture}
-            width={96}
-            height={96}
-            alt="profile_picture"
-            className="rounded-lg"
-          />
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="flex items-center flex-col gap-4 size"
+          >
+            {user?.picture && (
+              <Image
+                src={user.picture}
+                width={96}
+                height={96}
+                alt="profile_picture"
+                className="rounded-lg"
+              />
+            )}
+            <div className="flex flex-col items-center gap-1">
+              <span className="text-2xl font-semibold">
+                {user?.given_name} {user?.family_name}
+              </span>
+              <span className="text-zinc-600 text-sm">{user?.email}</span>
+            </div>
+          </motion.div>
         )}
-        <div className="flex flex-col items-center gap-1">
-          <span className="text-2xl font-semibold">
-            {user?.given_name} {user?.family_name}
-          </span>
-          <span className="text-zinc-600 text-sm">{user?.email}</span>
-        </div>
-
         <LogoutLink className="bg-primary rounded-full px-6 py-3 font-medium">
           Log out
         </LogoutLink>
