c4f3954bb5ad29aaedc7e11ede063a6bc34b836a
Anton Vo <antonlevo0903@gmail.com>
2025-01-21 22:46:18 +0100

    layout profile

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -12,31 +12,33 @@ const Profile = () => {
   const { user } = useKindeBrowserClient();
 
   return (
-    <div className="flex items-center flex-col gap-4h-full">
-      {!user?.picture ? (
-        <Spinner
-          size="size-8"
-          className="size-24 place-items-center place-content-center"
-        />
-      ) : (
-        <Image
-          src={user.picture}
-          width={96}
-          height={96}
-          alt="profile_picture"
-          className="rounded-lg"
-        />
-      )}
-      <div className="flex flex-col items-center gap-1">
-        <span className="text-2xl font-semibold">
-          {user?.given_name} {user?.family_name}
-        </span>
-        <span className="text-zinc-600 text-sm">{user?.email}</span>
-      </div>
+    <div className="flex items-center justify-evenly flex-col h-full">
+      <div className="flex items-center flex-col gap-4 ">
+        {!user?.picture ? (
+          <Spinner
+            size="size-8"
+            className="size-24 place-items-center place-content-center"
+          />
+        ) : (
+          <Image
+            src={user.picture}
+            width={96}
+            height={96}
+            alt="profile_picture"
+            className="rounded-lg"
+          />
+        )}
+        <div className="flex flex-col items-center gap-1">
+          <span className="text-2xl font-semibold">
+            {user?.given_name} {user?.family_name}
+          </span>
+          <span className="text-zinc-600 text-sm">{user?.email}</span>
+        </div>
 
-      <LogoutLink className="bg-primary rounded-full px-6 py-3 font-medium">
-        Log out
-      </LogoutLink>
+        <LogoutLink className="bg-primary rounded-full px-6 py-3 font-medium">
+          Log out
+        </LogoutLink>
+      </div>
 
       <RefreshButton />
     </div>
