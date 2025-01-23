23dbdede38f72807e9871061c343bc28aff5f385
Anton Vo <antonlevo0903@gmail.com>
2025-01-23 14:54:26 +0100

    fix hydration error

--- src/app/components/ProtectedRoute.tsx
+++ src/app/components/ProtectedRoute.tsx
@@ -1,11 +1,20 @@
 'use client';
 
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { useAuth } from '../hooks/useAuth';
 import Login from './Login';
 
 const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const { isLoggedIn } = useAuth();
+  const [isMounted, setIsMounted] = useState(false);
+
+  // Set isMounted to true after the component has mounted on the client
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  // Avoid rendering protected content until client-side hydration is complete
+  if (!isMounted) return null;
 
   return isLoggedIn ? (
     <main className="min-h-screen">{children}</main>
