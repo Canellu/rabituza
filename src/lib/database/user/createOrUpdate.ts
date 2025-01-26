b1877bf73a40a6fb7018bc28e5bc3b7ec2c6259e
Anton Vo <antonlevo0903@gmail.com>
2025-01-26 20:13:51 +0100

    rm merge true, data has to include old or it will overwrite

--- src/lib/database/user/createOrUpdate.ts
+++ src/lib/database/user/createOrUpdate.ts
@@ -5,7 +5,7 @@ import { db } from '../firebaseConfig';
 // Function to create or update user in Firestore
 export const createOrUpdateUser = async (user: User): Promise<void> => {
   try {
-    await setDoc(doc(db, 'users', user.id), user, { merge: true });
+    await setDoc(doc(db, 'users', user.id), user);
   } catch (error) {
     throw error;
   }
