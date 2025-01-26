ca0356313011ded426b25f5cd28bae54b7d0881a
Anton Vo <antonlevo0903@gmail.com>
2025-01-26 23:30:27 +0100

    add empty homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,27 @@
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { cn } from '@/lib/utils/tailwind';
-import { Goal, ListCheck, User } from 'lucide-react';
+import { Goal, HomeIcon, ListCheck, User } from 'lucide-react';
 import Goals from './components/Goals';
+import Home from './components/Home';
 import Profile from './components/Profile';
 import Tracker from './components/Tracker';
 import useVibrate from './hooks/useVibrate';
 
 enum Tab {
+  Home = 'home',
   Tracker = 'tracker',
   Goals = 'goals',
   Profile = 'profile',
 }
 
 const tabs = [
+  {
+    value: Tab.Home,
+    icon: HomeIcon,
+    title: 'Home',
+    content: <Home />,
+  },
   {
     value: Tab.Tracker,
     icon: ListCheck,
@@ -41,7 +49,7 @@ const Menu = () => {
   const vibrate = useVibrate(vibrationPattern);
 
   return (
-    <Tabs defaultValue={Tab.Goals}>
+    <Tabs defaultValue={Tab.Home}>
       {/* Tab Headers */}
       <TabsList className="backdrop-blur-sm gap-1 fixed inset-x-0 bottom-0 w-full min-h-max flex items-center justify-around pb-4 pt-0 rounded-none bg-gradient-to-b from-stone-800 to-stone-950">
         {tabs.map((tab) => (
