cbc86f18c164c73510688bfbe684be9f9a218114
Anton Vo <63739484+Canellu@users.noreply.github.com>
2025-02-05 01:30:44 +0100

    chore: sort cards

--- src/app/components/Activities.tsx
+++ src/app/components/Activities.tsx
@@ -8,6 +8,7 @@ import { getSession } from '@/lib/utils/userSession';
 import { useQuery } from '@tanstack/react-query';
 import { format } from 'date-fns';
 
+import BOULDERING_GYMS from '@/constants/boulderingGyms';
 import Spinner from './Spinner';
 
 const Activities = () => {
@@ -43,39 +44,56 @@ const Activities = () => {
           return (
             <div
               key={activity.id}
-              className="border rounded-lg p-4 shadow-sm space-y-2 bg-secondary"
+              className="border rounded-lg p-4 shadow-sm space-y-3 bg-secondary"
             >
               <div className="flex items-center justify-between">
                 <div className="flex items-center gap-2">
                   {Icon && (
-                    <Icon className="text-primary size-7 rounded-md bg-stone-100 border p-1" />
+                    <Icon className="text-primary size-7 rounded-md bg-gradient-to-br from-stone-50 to-stone-100 border p-1" />
                   )}
-                  <span className="font-medium">
+                  <span className="font-semibold inter text-stone-700">
                     {activityOptions.find((opt) => opt.id === type)?.label}
                   </span>
                 </div>
-                <span className="text-sm text-muted-foreground">
+                <span className="text-xs text-muted-foreground">
                   {activityDate && format(activityDate, 'PP, HH:mm')}
                 </span>
               </div>
 
               {/* Render activity-specific details */}
               {activity.type === 'bouldering' && (
-                <div className="text-sm text-muted-foreground capitalize">
-                  <p>Gym: {activity.gym}</p>
-                  <div className="flex flex-wrap gap-2 mt-2">
-                    {activity.grades.map((grade, idx) => (
-                      <span
-                        key={idx}
-                        className={cn(
-                          'bg-secondary px-2 py-1 rounded-md text-xs',
-                          getGradeColor(grade.grade).text,
-                          getGradeColor(grade.grade).bg
-                        )}
-                      >
-                        {grade.grade} × {grade.count}
-                      </span>
-                    ))}
+                <div className="flex items-start flex-col text-sm text-muted-foreground capitalize gap-3">
+                  <p className="text-xs font-medium border border-stone-200/70 px-3 py-1 bg-stone-50 max-w-max rounded-sm flex items-center gap-1">
+                    {activity.gym}
+                  </p>
+                  <div className="flex flex-wrap gap-1.5">
+                    {activity.grades
+                      .sort((a, b) => {
+                        const gymGrades =
+                          BOULDERING_GYMS[
+                            activity.gym as keyof typeof BOULDERING_GYMS
+                          ].grades;
+                        return (
+                          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                          // @ts-expect-error
+                          gymGrades.indexOf(a.grade) -
+                          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                          // @ts-expect-error
+                          gymGrades.indexOf(b.grade)
+                        );
+                      })
+                      .map((grade, idx) => (
+                        <span
+                          key={idx}
+                          className={cn(
+                            'px-2 py-0.5 rounded-sm text-xs',
+                            getGradeColor(grade.grade).text,
+                            getGradeColor(grade.grade).bg
+                          )}
+                        >
+                          {grade.grade} × {grade.count}
+                        </span>
+                      ))}
                   </div>
                 </div>
               )}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist, Geist_Mono, Inter } from 'next/font/google';
 import ProtectedRoute from './components/ProtectedRoute';
 import { Providers } from './components/Providers';
 import SplashScreen from './components/SplashScreen';
@@ -12,6 +12,11 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+const inter = Inter({
+  variable: '--font-inter',
   subsets: ['latin'],
 });
 
@@ -81,7 +86,7 @@ export default async function RootLayout({
         />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-stone-50 dark:bg-stone-900`}
+        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased bg-stone-50 dark:bg-stone-900`}
       >
         <Providers>
           <ProtectedRoute>
