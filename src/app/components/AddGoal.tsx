2a71d149227a3b547110a017ac02aee95ae6ad42
Anton Vo <63739484+Canellu@users.noreply.github.com>
2025-02-07 00:55:22 +0100

    test dialog

--- src/app/components/AddGoal.tsx
+++ src/app/components/AddGoal.tsx
@@ -24,6 +24,12 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { Dispatch, SetStateAction, useState } from 'react';
 import AnimateHeight from './AnimateHeight';
 import RequiredStar from './RequiredStar';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
 
 const categories = [
   'Health',
@@ -119,142 +125,144 @@ const AddGoal = ({ isEditing, setIsEditing, setActiveTab }: AddGoalProps) => {
   };
 
   return (
-    <AnimateHeight isOpen={isEditing}>
-      <section className="px-6 pt-8 pb-12 flex flex-col items-center gap-6 overflow-auto bg-white border border-input rounded-md">
-        <div className="flex flex-col gap-2 ">
-          <h2 className="text-lg font-semibold text-stone-800">
-            Add a New Goal
-          </h2>
-          <p className="text-stone-600 text-sm">
-            Set a new goal to help you stay focused and motivated.
-          </p>
-        </div>
+    <Dialog open={isEditing} onOpenChange={setIsEditing}>
+      <DialogContent className="max-w-lg w-full max-h-[80vh] overflow-y-auto">
+        <DialogHeader>
+          <DialogTitle>Add a New Goal</DialogTitle>
+        </DialogHeader>
+        <div className="flex flex-col items-center gap-6">
+          <div className="flex flex-col gap-2">
+            <p className="text-stone-600 text-sm">
+              Set a new goal to help you stay focused and motivated.
+            </p>
+          </div>
 
-        {/* Title */}
-        <div className="flex flex-col-reverse w-full gap-2">
-          <Input
-            type="text"
-            id="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="Goal title"
-          />
-          <Label htmlFor="title">
-            Title
-            <RequiredStar />
-          </Label>
-        </div>
+          {/* Title */}
+          <div className="flex flex-col-reverse w-full gap-2">
+            <Input
+              type="text"
+              id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Goal title"
+            />
+            <Label htmlFor="title">
+              Title
+              <RequiredStar />
+            </Label>
+          </div>
 
-        {/* Description */}
-        <div className="flex flex-col-reverse w-full gap-2">
-          <Input
-            type="text"
-            id="description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            placeholder="Goal description"
-          />
-          <Label htmlFor="description">Description</Label>
-        </div>
+          {/* Description */}
+          <div className="flex flex-col-reverse w-full gap-2">
+            <Input
+              type="text"
+              id="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="Goal description"
+            />
+            <Label htmlFor="description">Description</Label>
+          </div>
 
-        {/* Category */}
-        <div className="flex flex-col-reverse w-full gap-2">
-          <Select
-            value={category}
-            onValueChange={(category) => setCategory(category)}
-          >
-            <SelectTrigger
+          {/* Category */}
+          <div className="flex flex-col-reverse w-full gap-2">
+            <Select
+              value={category}
+              onValueChange={(category) => setCategory(category)}
+            >
+              <SelectTrigger
+                className={cn(
+                  'w-full',
+                  category ? 'text-stone-800' : 'text-muted-foreground'
+                )}
+                id="category"
+              >
+                <SelectValue placeholder="Category" />
+              </SelectTrigger>
+              <SelectContent className="z-[1000]">
+                {categories.map((category) => (
+                  <SelectItem key={category} value={category}>
+                    {category}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+            <Label htmlFor="category">
+              Category
+              <RequiredStar />
+            </Label>
+          </div>
+
+          {/* Tags */}
+          <div className="flex flex-col-reverse w-full gap-2">
+            <MultiSelect
+              maxSelected={6}
+              options={tagsList}
+              value={tags}
+              placeholder="Select or create your own tags"
+              creatable
+              onChange={(tags) => {
+                setTags(tags);
+              }}
+            />
+            <Label htmlFor="tags">Tags</Label>
+          </div>
+
+          {/* Period */}
+          <div className="flex flex-col-reverse w-full gap-2">
+            <Select
+              value={timePeriod}
+              onValueChange={(timePeriod: TimePeriod) =>
+                setTimePeriod(timePeriod)
+              }
+            >
+              <SelectTrigger
+                className={cn(
+                  'w-full',
+                  timePeriod ? 'text-stone-800' : 'text-muted-foreground'
+                )}
+                id="timePeriod"
+              >
+                <SelectValue placeholder="Year" />
+              </SelectTrigger>
+              <SelectContent className="z-[1000]">
+                {timePeriods.map((timePeriod) => (
+                  <SelectItem key={timePeriod} value={timePeriod}>
+                    {timePeriod}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+            <Label htmlFor="timePeriod">Time Period</Label>
+          </div>
+
+          <div className="flex flex-col w-full gap-6">
+            <p
               className={cn(
-                'w-full',
-                category ? 'text-stone-800' : 'text-muted-foreground'
+                ' text-red-500 bg-red-100 rounded-md py-1 px-3 w-full text-center',
+                error ? 'block' : 'hidden'
               )}
-              id="category"
             >
-              <SelectValue placeholder="Category" />
-            </SelectTrigger>
-            <SelectContent className="z-[1000]">
-              {categories.map((category) => (
-                <SelectItem key={category} value={category}>
-                  {category}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-          <Label htmlFor="category">
-            Category
-            <RequiredStar />
-          </Label>
-        </div>
+              {error}
+            </p>
 
-        {/* Tags */}
-        <div className="flex flex-col-reverse w-full gap-2">
-          <MultiSelect
-            maxSelected={6}
-            options={tagsList}
-            value={tags}
-            placeholder="Select or create your own tags"
-            creatable
-            onChange={(tags) => {
-              setTags(tags);
-            }}
-          />
-          <Label htmlFor="tags">Tags</Label>
-        </div>
-
-        {/* Period */}
-        <div className="flex flex-col-reverse w-full gap-2">
-          <Select
-            value={timePeriod}
-            onValueChange={(timePeriod: TimePeriod) =>
-              setTimePeriod(timePeriod)
-            }
-          >
-            <SelectTrigger
-              className={cn(
-                'w-full',
-                timePeriod ? 'text-stone-800' : 'text-muted-foreground'
-              )}
-              id="timePeriod"
-            >
-              <SelectValue placeholder="Year" />
-            </SelectTrigger>
-            <SelectContent className="z-[1000]">
-              {timePeriods.map((timePeriod) => (
-                <SelectItem key={timePeriod} value={timePeriod}>
-                  {timePeriod}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-          <Label htmlFor="timePeriod">Time Period</Label>
-        </div>
-
-        <div className="flex flex-col w-full gap-6">
-          <p
-            className={cn(
-              ' text-red-500 bg-red-100 rounded-md py-1 px-3 w-full text-center',
-              error ? 'block' : 'hidden'
-            )}
-          >
-            {error}
-          </p>
-
-          {/* Save and Cancel buttons */}
-          <div className="flex items-center justify-between w-full">
-            <Button
-              onClick={() => setIsEditing(false)}
-              variant="secondary"
-              disabled={!isEditing}
-            >
-              Cancel
-            </Button>
-            <Button onClick={handleSave} disabled={!isEditing}>
-              Save
-            </Button>
+            {/* Save and Cancel buttons */}
+            <div className="flex items-center justify-between w-full">
+              <Button
+                onClick={() => setIsEditing(false)}
+                variant="secondary"
+                disabled={!isEditing}
+              >
+                Cancel
+              </Button>
+              <Button onClick={handleSave} disabled={!isEditing}>
+                Save
+              </Button>
+            </div>
           </div>
         </div>
-      </section>
-    </AnimateHeight>
+      </DialogContent>
+    </Dialog>
   );
 };
 
