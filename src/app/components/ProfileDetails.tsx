06dc6e92fa8f5b741972bd85fb1929576c907131
Anton Vo <63739484+Canellu@users.noreply.github.com>
2025-02-07 01:07:52 +0100

    update text color profile details

--- src/app/components/ProfileDetails.tsx
+++ src/app/components/ProfileDetails.tsx
@@ -352,7 +352,7 @@ const ProfileDetails = ({ user }: { user?: User }) => {
                   className={cn(
                     isBio
                       ? 'text-secondary-foreground whitespace-pre-line p-4 border rounded-md bg-stone-50 shadow-inner'
-                      : 'text-emerald-600 font-medium text-right'
+                      : 'text-emerald-700 font-medium text-right'
                   )}
                 >
                   {formattedValue}
