49f0a039359d3882617e82a6645db6fe91cfa697
Anton Vo <63739484+Canellu@users.noreply.github.com>
2025-02-12 15:44:30 +0100

    test back navigation

--- src/app/(other pages)/feedback/page.tsx
+++ src/app/(other pages)/feedback/page.tsx
@@ -5,8 +5,26 @@ import { Button } from '@/components/ui/button';
 import { Tab } from '@/constants/menu';
 import { ArrowLeft } from 'lucide-react';
 import Link from 'next/link';
+import { useEffect } from 'react';
+import { toast } from 'sonner';
 
 const FeedbackPage = () => {
+  useEffect(() => {
+    const handlePopState = (event: PopStateEvent) => {
+      // Perform an action before back navigation
+      toast('back navigation');
+      // You can add any logic here, such as showing a confirmation dialog
+    };
+
+    // Listen for the popstate event
+    window.addEventListener('popstate', handlePopState);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('popstate', handlePopState);
+    };
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center h-dvh">
       <Button
